f22b9be5ca5fe6fd0ac14f6da1ac38dac6b5cce5
Gowri Chandana PB <gowrichandana85@gmail.com>
2026-02-18 15:27:04 +0530

    Add fruit weight aggregation script
    This script aggregates sold weights of fruits from a list of dictionaries into a single dictionary.

---
+++ codes/feb_18.py
@@ -0,0 +1,25 @@
+
+# input
+# dist = [{"fruit_name":"apple", "sold_weight": 10},
+# {"fruit_name":"banana", "sold_weight": 20},
+# {"fruit_name":"apple", "sold_weight": 30},
+# {"fruit_name":"banana", "sold_weight": 40},
+# {"fruit_name":"orange", "sold_weight": 50}]
+
+# output
+# dist = {"apple": 40, "banana": 60, "orange": 50}
+ 
+dist = [{"fruit_name":"apple", "sold_weight": 10},
+{"fruit_name":"banana", "sold_weight": 20},
+{"fruit_name":"apple", "sold_weight": 30},
+{"fruit_name":"banana", "sold_weight": 40},
+{"fruit_name":"orange", "sold_weight": 50}]
+result = {}
+for i in dist:
+    fruit_name = i["fruit_name"]
+    sold_weight = i["sold_weight"]
+    if fruit_name in result:
+        result[fruit_name] += sold_weight
+    else:
+        result[fruit_name] = sold_weight
+print(result)
